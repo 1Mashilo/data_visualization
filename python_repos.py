5e8eb7ba76bb365351aed47df4467fe81898552b
1Mashilo <christophermashilo@gmail.com>
2023-12-25 23:48:33 +0100

    Enhanced GitHub API script with error handling and additional result information

---
+++ python_repos.py
@@ -0,0 +1,42 @@
+import requests
+
+# Make an API call and check the response.
+url = "https://api.github.com/search/repositories"
+url += "?q=language:python+sort:stars+stars:>10000"
+headers = {"Accept": "application/vnd.github.v3+json"}
+
+try:
+    # Send HTTP GET request
+    r = requests.get(url, headers=headers)
+    
+    # Check if the request was successful
+    r.raise_for_status()
+
+    # Print the status code
+    print(f"Status code: {r.status_code}")
+
+    # Convert the response object to a dictionary.
+    response_dict = r.json()
+
+    # Process results.
+    print(f"Total repositories: {response_dict['total_count']}")
+    print(f"Complete results: {not response_dict['incomplete_results']}")
+
+    # Explore information about the repositories.
+    repo_dicts = response_dict['items']
+    print(f"Repositories returned: {len(repo_dicts)}")
+
+    # Examine the first repository.
+    repo_dict = repo_dicts[0]
+    print(f"\nKeys: {len(repo_dict)}")
+    for key in sorted(repo_dict.keys()):
+        print(key)
+
+except requests.exceptions.HTTPError as errh:
+    print(f"HTTP Error: {errh}")
+except requests.exceptions.ConnectionError as errc:
+    print(f"Error Connecting: {errc}")
+except requests.exceptions.Timeout as errt:
+    print(f"Timeout Error: {errt}")
+except requests.exceptions.RequestException as err:
+    print(f"An error occurred: {err}")
