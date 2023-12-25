0d6bb4af76c578deefa279e5bb75973689239d20
1Mashilo <christophermashilo@gmail.com>
2023-12-26 00:02:41 +0100

    .

--- python_repos.py
+++ python_repos.py
@@ -27,20 +27,16 @@ try:
     print(f"Repositories returned: {len(repo_dicts)}")
 
     # Examine the first repository.
-    repo_dict = repo_dicts[0]
-    print(f"\nKeys: {len(repo_dict)}")
-    for key in sorted(repo_dict.keys()):
-        print(key)
-
-    # Selected information about the first repository.
-    print("\nSelected information about the first repository:")
-    print(f"Name: {repo_dict['name']}")
-    print(f"Owner: {repo_dict['owner']['login']}")
-    print(f"Stars: {repo_dict['stargazers_count']}")
-    print(f"Repository: {repo_dict['html_url']}")
-    print(f"Created: {repo_dict['created_at']}")
-    print(f"Updated: {repo_dict['updated_at']}")
-    print(f"Description: {repo_dict['description']}")
+    print("\nSelected information about each repository:")
+    for repo_dict in repo_dicts:
+        print("\nSelected information about each repository:")
+        print(f"Name: {repo_dict['name']}")
+        print(f"Owner: {repo_dict['owner']['login']}")
+        print(f"Stars: {repo_dict['stargazers_count']}")
+        print(f"Repository: {repo_dict['html_url']}")
+        print(f"Created: {repo_dict['created_at']}")
+        print(f"Updated: {repo_dict['updated_at']}")
+        print(f"Description: {repo_dict['description']}")
 
 except requests.exceptions.HTTPError as errh:
     print(f"HTTP Error: {errh}")
